cfa9b2affe99925a1e2be2224cf2b5d9c6afd042
Admin <Admin@COMP>
2026-05-18 22:11:21 +0400

    добавлена карточка растения

---
+++ Views/Plants/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace greenhome.Views.Plants
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
